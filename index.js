9980a8e44f879de00360186e1f4c6b8810287314
biatrix1995 <b.cordeiroaraujo@gmail.com>
2022-09-15 18:32:41 -0300

    criação de pagina index com HTML e JAVA

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Tipos primitivos
+ * Variáveis no Javascript
+ * Operadores matemáticos do Javascript
+ */
+
+// serve para escrever mensagens no console
+// string - textos
+// number - qualquer tipo de número (decimal ou inteiro)
+// boolean - booleano verdadeiro ou falso
+
+console.log ("Hello world!")
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+//variáveis
+let mensagem = "SoulCode BC25 JFS"
+
+console.log(mensagem)
+
+mensagem = "Novo texto"
+
+console.log(mensagem)
